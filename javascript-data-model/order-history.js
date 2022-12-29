14aa4b3470077574ab15818a1e838170d878c73a
Marianne Cuevas <mariannecuevas97@gmail.com>
2022-12-29 06:22:24 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,40 @@
+var orderHistory = [
+  {
+    'order #': '114-3941689-8772232',
+    'order placed': '08/4/2020',
+    'total items': 1,
+    products: 'JavaScript for impatient programmers',
+    total: '$34.00',
+    delivered: '08/8/2020',
+    'ship to': 'JS Masher'
+  },
+  {
+    'order #': '113-9984268-1280257',
+    'order placed': '07/19/2020',
+    'total items': 1,
+    products: 'The Timeless Way of Building',
+    total: '$44.53',
+    delivered: '07/20/2020',
+    'ship to': 'JS Masher'
+  },
+  {
+    'order #': '114-2875557-9059409',
+    'order placed': '07/4/2020',
+    'total items': 1,
+    products: 'Gamecube Controller Adapter',
+    total: '$17.22',
+    delivered: '07/7/2020',
+    'ship to': 'JS Masher'
+  },
+  {
+    'order #': '113-2883177-2648248',
+    'order placed': '07/3/2020',
+    'total items': 2,
+    products: ['GameCube Controller', 'The Art of Sql'],
+    total: '$138.93',
+    delivered: '07/5/2020',
+    'ship to': 'JS Masher'
+  }
+];
+
+console.log('value of orderHistory:', orderHistory);
