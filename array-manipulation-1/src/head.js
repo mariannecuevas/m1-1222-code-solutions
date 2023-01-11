31432b4a45f1420cb8bd3c67b72af414835600b2
Marianne Cuevas <mariannecuevas97@gmail.com>
2023-01-11 05:19:10 +0000

    Completed array-manipulation-1 challenge.

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,8 @@
+/* exported head */
+// Define a function named head that accepts one argument, array
+//    Declare a variable firstElement and assign it to the ordered list at index 0
+//    Return the value of firstElement variable
+function head(array) {
+  var firstElement = array[0];
+  return firstElement;
+}
