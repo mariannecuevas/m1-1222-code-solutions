01c3b36962f9dda9d355fbc893b09a157e3cfc79
Marianne Cuevas <mariannecuevas97@gmail.com>
2023-01-19 03:59:00 +0000

    Fixed the code in setValue.

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -3,6 +3,5 @@
 //    Declare a variable named 'keyValue', and asisgn it to the result of the expression object at key = value.
 //    Return the value of the keyValue variable.
 function setValue(object, key, value) {
-  var keyValue = object[key] = value;
-  return keyValue;
+  object[key] = value;
 }
