f663ad70bc4dda190edc4a86123d7bb0748775c7
Marianne Cuevas <mariannecuevas97@gmail.com>
2023-02-02 10:06:05 +0000

    Added vowels to an array, and checked if the char includes any of the values from the vowels list.

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,35 +1,19 @@
 /* exported isVowel */
-// Check if char is a string or if the length of char is greater than 2,
-// If it is not a string or if the length of char is greater than 2, return 'Invalid string'
-// If it is a string,
-// Check if the character is 'a'
-// if true, return 'true'
-// Check if the character is 'e'
-// if true, return 'true'
-// Check if the character is 'i'
-// if true, return 'true'
-// Check if the character is 'o'
-// if true, return 'true'
-// Check if the character is 'u'
-// if true, return 'true'
-// Check if the character is 'A'
-// if true, return 'true'
-// Check if the character is 'E'
-// if true, return 'true'
-// Check if the character is 'I'
-// if true, return 'true'
-// Check if the character is 'O'
-// if true, return 'true'
-// Check if the character is 'U'
-// if true, return 'true'
-// If character does not match any of the conditions above
-// then return 'false'
+//    Define a function named 'isVowel' which takes one argument, char.
+//        Declare a variable named 'vowels' and assign it to a list of all the vowels, lowercase and uppercase.
+//            Check if the char includes any of the vowels from the vowels list
+//                If this condition is true, return true.
+//            Check if the typeof char is NOT string or if the char length is greater than 2
+//                If this condition is true, return Invalid String
+//            If the char does not include a vowel from the vowels list, return false.
 function isVowel(char) {
-  if (typeof char !== 'string' || char.length > 2) {
-    return 'Invalid string';
-  } else if (char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'i' || char === 'I' || char === 'o' || char === 'O' || char === 'u' || char === 'U') {
-    return true;
-  } else {
-    return false;
+  var vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
+  for (var i = 0; i < vowels.length; i++) {
+    if (char.includes(vowels[i])) {
+      return true;
+    } else if (typeof char !== 'string' || char.length > 2) {
+      return 'Invalid string';
+    }
   }
+  return false;
 }
