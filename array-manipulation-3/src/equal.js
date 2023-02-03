6c05c96b3a6b1130e0fbc91fc1299fab22ecc151
Marianne Cuevas <mariannecuevas97@gmail.com>
2023-02-03 05:33:27 +0000

    Removed else. Cleaned up code.

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -10,12 +10,12 @@
 function equal(first, second) {
   if (first.length !== second.length) {
     return false;
-  } else {
-    for (var i = 0; i < first.length; i++) {
-      if (first[i] !== second[i]) {
-        return false;
-      }
+  }
+  for (var i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
     }
   }
+
   return true;
 }
